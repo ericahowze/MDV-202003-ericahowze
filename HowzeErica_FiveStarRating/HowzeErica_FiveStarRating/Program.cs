570be2bd15a8b730457b6d3494caaf72f27838dd
Erica Howze <EMHowze@student.fullsail.edu>
2020-03-12 22:07:07 -0400

    Created project for Data Visualization Practice

---
+++ HowzeErica_FiveStarRating/HowzeErica_FiveStarRating/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowzeErica_FiveStarRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
