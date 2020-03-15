dc3ce86bc01afa0a6b61d1f1d2e8de2711a1f104
Erica Howze <EMHowze@student.fullsail.edu>
2020-03-15 18:57:50 -0400

    Moved pdf file to repo folder

--- HowzeErica_FiveStarRating/HowzeErica_FiveStarRating/Program.cs
+++ HowzeErica_FiveStarRating/HowzeErica_FiveStarRating/Program.cs
@@ -10,9 +10,81 @@ namespace HowzeErica_FiveStarRating
     {
         static void Main(string[] args)
         {
+            bool isProgramRunning = true;
 
-            //prompt user for choice
-            Console.WriteLine("");
+            while (isProgramRunning)
+            {
+
+                Console.Clear();
+                //prompt user for choice
+                Console.WriteLine("Welcome to my Restaurant Rating System" +
+                    "\nWhat would you like to do today?" +
+                    "\n1). Convert The Restaurant Profile Table From SQL To JSON" +
+                    "\n2). Showcase Our 5 Star Rating System" +
+                    "\n3). Showcase Our Animated Bar Graph Review System" +
+                    "\n4). Play A Card Game" +
+                    "\n5). Exit");
+
+                string choice = Validation.ValidateSentence("Choose an option to continue ");
+
+                switch (choice)
+                {
+                    case "1":
+                    case "convert to json":
+                        {
+
+                        }
+                        break;
+                    case "2":
+                    case "show stars":
+                        {
+
+                        }
+                        break;
+                    case "3":
+                    case "show bars":
+                        {
+
+                        }
+                        break;
+                    case "4":
+                    case "play card game":
+                        {
+
+                        }
+                        break;
+                    case "5":
+                    case "exit":
+                        {
+                            Console.WriteLine("Thank you for shopping with us");
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("You chose an invalid option, please choose from the menu");
+                        Console.WriteLine("Press the return key to return to the main menu");
+                        Console.ReadKey();
+                        return;
+                }
+            }
+        }
+
+        public static void ConvertedData()
+        {
+
+        }
+
+        public static void ShowStars()
+        {
+
+        }
+
+        public static void ShowBar()
+        {
+
+        }
+
+        public static void PlayCardGame()
+        {
 
         }
     }
--- HowzeErica_FiveStarRating/HowzeErica_FiveStarRating/Validation.cs
+++ HowzeErica_FiveStarRating/HowzeErica_FiveStarRating/Validation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EricaHowze_ce09
+namespace HowzeErica_FiveStarRating
 {
     class Validation
     {
